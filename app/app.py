693674bab4aa6d4c51cd84b6db67454b80953d9c
Minas <93990109+Minas-S@users.noreply.github.com>
2025-12-12 21:33:31 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "ip": request.remote_addr, "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+})
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", port=5000)
